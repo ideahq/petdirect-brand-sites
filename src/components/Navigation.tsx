5bdf005c62d64cc01ffe9d45da30588523a81f36
Leo Rimmer <leo@petdirect.co.nz>
2025-06-11 16:05:55 +1200

    add navigation

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
+
+interface NavigationProps {
+    logo: string;
+    name: string;
+    links: {
+        name: string;
+        url: string;
+    }[];
+}
+
+const Navigation: NavigationProps = ({...props}) => {
+    return (
+        <Navbar className="p-3" expand="md">
+            <Navbar.Brand href="/">
+                <img class="logo" src={`${props.id}/logo.svg`} alt={props.name} />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbar-nav" />
+            <Navbar.Collapse id="navbar-nav">
+                <Nav className="ms-auto pt-3 pt-md-0">
+                    {props.links.map((link, i) =>
+                        <Nav.Link
+                            key={i}
+                            href={link.url}
+                        >
+                            {link.name}
+                        </Nav.Link>
+                    )}
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+}
+
+export default Navigation;
