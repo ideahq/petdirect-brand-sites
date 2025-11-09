d1703a0c3f1975756cec3f8df7e035ec312c1b95
Leo Rimmer <leo@petdirect.co.nz>
2025-11-10 09:46:32 +1300

    remove trailing slash from main url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://wildpaw.co.nz/',
+    site: 'https://wildpaw.co.nz',
     integrations: [react()],
     env: {
         schema: {
