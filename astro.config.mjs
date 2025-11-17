1208ab56f04135ee480ea3c618ec3635484767de
Leo Rimmer <leo@petdirect.co.nz>
2025-11-18 08:54:35 +1300

    env var based sass imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,17 @@ import { defineConfig, envField } from 'astro/config';
 
 import react from '@astrojs/react';
 
+const THEME = process.env.THEME || 'wild-paw';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://wildpaw.co.nz',
     integrations: [react()],
+    vite: {
+        define: {
+            '__THEME__': JSON.stringify(THEME)
+        }
+    },
     env: {
         schema: {
             THEME: envField.enum({
